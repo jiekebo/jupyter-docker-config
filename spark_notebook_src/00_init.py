3188d0c449cf6c0bdbd54d8cef86744a06efea63
Jacob Salomonsen <jiekebo@gmail.com>
2019-09-14 07:36:07 +0200

    Setting maxResultSize higher

--- spark_notebook_src/00_init.py
+++ spark_notebook_src/00_init.py
@@ -4,6 +4,7 @@ spark = (SparkSession.builder
  .appName("local app")
  .master("local[*]")
  .config('spark.driver.memory','7g')
+ .config('spark.driver.maxResultSize','5g')
  .config('spark.jars.packages','com.databricks:spark-avro_2.11:3.2.0')
 ).getOrCreate()
 
