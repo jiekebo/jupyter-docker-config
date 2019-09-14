650bde0b4d84cabd8d82d67d8a19dbbc2f1de482
Jacob Salomonsen <jiekebo@gmail.com>
2019-09-14 06:38:36 +0200

    Re-added parameters to start_spark_notebook script

--- start_spark_notebook.sh
+++ start_spark_notebook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker run -it --rm --name jupyter-spark-notebook -p 8888:8888 -p 4040:4040 -p 4041:4041 -e GRANT_SUDO="yes" --user root \
-    -v ~/Dropbox/notebooks:/home/jovyan/notebooks \
-    -v ~/Dropbox/Data:/home/jovyan/data \
+docker run -it --rm --name jupyter-spark-notebook -p 8888:8888 -p 4040-4045:4040-4045 -e GRANT_SUDO="yes" --user root \
+    -v $1:/home/jovyan/notebooks \
+    -v $2:/home/jovyan/data \
     jupyter-spark-notebook start-notebook.sh \
     --NotebookApp.password='sha1:0400c91489bd:4ddcf82fc658b043fc4a758261c34636801ad5df'
